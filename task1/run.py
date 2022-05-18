7f8ba45e487aa7908ffec937c7929cc85e9a74b1
Avulapati Niranjan <45714236+niranjan1531@users.noreply.github.com>
2022-05-18 19:27:58 +0530

    Update run.py

--- task1/run.py
+++ task1/run.py
@@ -1,8 +1,10 @@
 import time
+import subprocess
 
+subprocess.run(["ls", "-l"])
+subprocess.run(["docker", "ps"])
 print('Start task 1')
 print(time.ctime())
- 
 # using sleep() to hault the code execution
 time.sleep(10)
 
