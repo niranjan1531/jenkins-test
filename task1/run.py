a11837d34f49a54e6faae859ecf301ed7fd8e953
Avulapati Niranjan <45714236+niranjan1531@users.noreply.github.com>
2022-05-19 10:51:34 +0530

    Update run.py

--- task1/run.py
+++ task1/run.py
@@ -4,8 +4,8 @@ import os
 
 
 subprocess.run(["ls", "-l"])
-os.system('docker ps')
-subprocess.call("docker ps -a", shell=True)
+#os.system('docker ps')
+#subprocess.call("docker ps -a", shell=True)
 print('Start task 1')
 print(time.ctime())
 # using sleep() to hault the code execution
